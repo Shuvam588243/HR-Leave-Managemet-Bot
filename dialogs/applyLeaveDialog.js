44e827b8cfa55436d6862c1a551cd5b05a05274a
Shuvam Sengupta <shuvamsengupta58824@gmail.com>
2021-10-21 02:26:23 -0700

    First Push

--- dialogs/applyLeaveDialog.js
+++ dialogs/applyLeaveDialog.js
@@ -37,12 +37,12 @@ class ApplyLeave extends ComponentDialog{
         this.addDialog(new NumberPrompt(NumberPromptDialog));
         this.addDialog(new TextPrompt(TextPromptDialog));
 
-        this.addDialog(new WaterfallDialog(applyLeaveDialogWaterfall1,[
-            this.askLeaveType.bind(this),
-            this.askNoOfDays.bind(this),
-            this.askDateOfLeave.bind(this),
-            this.leaveConfirmation.bind(this)
-        ]))
+        // this.addDialog(new WaterfallDialog(applyLeaveDialogWaterfall1,[
+        //     this.askLeaveType.bind(this),
+        //     this.askNoOfDays.bind(this),
+        //     this.askDateOfLeave.bind(this),
+        //     this.leaveConfirmation.bind(this)
+        // ]))
 
         this.addDialog(new WaterfallDialog(applyLeaveDialogWaterfall2,[
             this.showForm.bind(this),
@@ -55,79 +55,94 @@ class ApplyLeave extends ComponentDialog{
         this.initialDialogId = applyLeaveDialogWaterfall2
     }
 
-    async askLeaveType(stepContext){
-        return await stepContext.prompt(ChoicePromptDialog,{
-            prompt : 'Please help me with the type of leave you want to apply for',
-            choices : ChoiceFactory.toChoices([
-                'Sick Leave',
-                'Casual Leave',
-                'Earned Leave'
-            ])
-        })
-    }
+    // async askLeaveType(stepContext){
+    //     return await stepContext.prompt(ChoicePromptDialog,{
+    //         prompt : 'Please help me with the type of leave you want to apply for',
+    //         choices : ChoiceFactory.toChoices([
+    //             'Sick Leave',
+    //             'Casual Leave',
+    //             'Earned Leave'
+    //         ])
+    //     })
+    // }
 
-    async askNoOfDays(stepContext){
-        let dialogState = await this.applyLeaveDataAccessor.get(stepContext.context,{});
-        dialogState.leaveType = stepContext.result.value;
-        return await stepContext.prompt(NumberPromptDialog,`For How many days you want to apply for ${dialogState.leaveType}`);
-    }
+    // async askNoOfDays(stepContext){
+    //     let dialogState = await this.applyLeaveDataAccessor.get(stepContext.context,{});
+    //     dialogState.leaveType = stepContext.result.value;
+    //     return await stepContext.prompt(NumberPromptDialog,`For How many days you want to apply for ${dialogState.leaveType}`);
+    // }
 
-    async askDateOfLeave(stepContext){
-        let dialogState = await this.applyLeaveDataAccessor.get(stepContext.context);
-        dialogState.leaveDays = stepContext.result;
-        return await stepContext.prompt(TextPromptDialog,`From which date you want to apply for you ${dialogState.leaveType} leave application of ${dialogState.leaveDays} days?`);
-    }
+    // async askDateOfLeave(stepContext){
+    //     let dialogState = await this.applyLeaveDataAccessor.get(stepContext.context);
+    //     dialogState.leaveDays = stepContext.result;
+    //     return await stepContext.prompt(TextPromptDialog,`From which date you want to apply for you ${dialogState.leaveType} leave application of ${dialogState.leaveDays} days?`);
+    // }
 
-    async leaveConfirmation(stepContext){
-        let dialogState = await this.applyLeaveDataAccessor.get(stepContext.context);
-        dialogState.leaveDate = stepContext.result;
-        await stepContext.context.sendActivity({
-            attachments : [
-                CardFactory.adaptiveCard(confirmLeave(
-                    dialogState.leaveType,
-                    dialogState.leaveDays,
-                    dialogState.leaveDate
-                ))
-            ]
-        })
-        return await stepContext.endDialog();
-    }
+    // async leaveConfirmation(stepContext){
+    //     let dialogState = await this.applyLeaveDataAccessor.get(stepContext.context);
+    //     dialogState.leaveDate = stepContext.result;
+    //     await stepContext.context.sendActivity({
+    //         attachments : [
+    //             CardFactory.adaptiveCard(confirmLeave(
+    //                 dialogState.leaveType,
+    //                 dialogState.leaveDays,
+    //                 dialogState.leaveDate
+    //             ))
+    //         ]
+    //     })
+    //     return await stepContext.endDialog();
+    // }
 
     //with form
     async showForm(stepContext){
         try{
-            await stepContext.context.sendActivity({
-                attachments : [
-                    CardFactory.adaptiveCard(leaveApplicationForm())
-                ]
-            })
-            return Dialog.EndOfTurn;
+           if(stepContext.options && stepContext.options.formRefill){
+               return stepContext.next();
+           }else{
+               await stepContext.context.sendActivity({
+                   attachments: [
+                       CardFactory.adaptiveCard(leaveApplicationForm())
+                   ]
+               });
+               return Dialog.EndOfTurn;
+           }
         }catch(error){
-            console.log(error)
+            console.log(error)  
         }
     }
     
     async preprocessUserInput(stepContext){
         try{
-            let userInput = stepContext.context.activity.value;
-            console.log(userInput);
-            let numberOfDays = userInput.leaveDays;
-            if(numberOfDays > 3){
-                await stepContext.context.sendActivity('Maximum Applicable Leave Days are 3. Please Contact HR in person for futher progress of the application')
+            let dialogState = await this.applyLeaveDataAccessor.get(stepContext.context,{});
+            let userInput;
+            if(stepContext.options && stepContext.options.formRefill){
+                userInput = stepContext.options.values;
             }else{
-                await stepContext.context.sendActivity({
-                    attachments : [
-                        CardFactory.adaptiveCard(confirmLeave(
-                            userInput.leaveType,
-                            userInput.leaveDays,
-                            userInput.leaveDate
-                        ))
-                    ]
-                })
-
-
-            }      
-            return stepContext.endDialog();
+                userInput = stepContext.context.activity.value;
+            }
+            console.log(userInput)
+            dialogState.leaveType = userInput.leaveType;
+            dialogState.leaveDays = userInput.leaveDays;
+            dialogState.leaveDate = userInput.leaveDate;
+            if(parseInt(dialogState.leaveDays) > 3){
+                await stepContext.context.sendActivity('You can only apply for 3 days of leave in a row. Please enter the number of details once again')
+            }else{
+                if(dialogState.leaveType && dialogState.leaveDays && dialogState.leaveDate){
+                    await stepContext.context.sendActivity({
+                        attachments : [
+                            CardFactory.adaptiveCard(confirmLeave(
+                                dialogState.leaveType,
+                                dialogState.leaveDays,
+                                dialogState.leaveDate
+                            ))
+                        ]
+                    })
+                }
+                else{
+                    await stepContext.context.sendActivity('Please Fill All the Fields to proceed');
+                }
+                return stepContext.next();
+            }
         }catch(error){
             console.log(error)
         }
--- dialogs/rootDialog.js
+++ dialogs/rootDialog.js
@@ -4,6 +4,7 @@ const {
     DialogSet,
     DialogTurnStatus
 } = require('botbuilder-dialogs');
+const { LuisRecognizer } = require('botbuilder-ai');
 
 
 //Dialogs
@@ -18,6 +19,12 @@ const {applyLeaveDialog} = require('../constants')
 const {leaveStatusDialog} = require('../constants');
 const rootDialogWaterfall1 = 'rootDialogWaterfall1'
 
+const luisConfig = {
+    applicationId : 'c2c209d2-1c08-497b-9313-afc137ad16bd',
+    endpointKey : '56a94745a1bf4cfbb01fbdb106053a33',
+    endpint : 'https://luisbottraining.cognitiveservices.azure.com/'
+}
+
 class RootDialog extends ComponentDialog{
     constructor(conversationState){
         super(rootDialog);
@@ -29,6 +36,10 @@ class RootDialog extends ComponentDialog{
             this.routeMessages.bind(this)
         ]))
 
+        this.recognizer = new LuisRecognizer(luisConfig, {
+            apiVersion : 'v3'
+        })
+
         this.addDialog(new HelpDialog(conversationState));
         this.addDialog(new ApplyLeaveDialog(conversationState));
         this.addDialog(new LeaveStatusDialog(conversationState));
@@ -37,6 +48,23 @@ class RootDialog extends ComponentDialog{
     }
 
     async routeMessages(stepContext){
+    console.log('stepContext.context.activity.activity => ', stepContext.context.activity);
+    if(stepContext.context.activity.value && stepContext.context.activity.value.actionType){
+        switch(stepContext.context.activity.value.actionType){
+            case 'applyLeaveApplicationAction':
+                let formvalues = stepContext.context.activity.value;
+                delete stepContext.context.activity.value;
+                return await stepContext.beginDialog(applyLeaveDialog,{
+                    formRefill:true,
+                    values:formvalues
+                })
+        }
+    }
+    else{
+        // let luisresponse = await this.recognizer.recognize(stepContext.context);
+        // console.log('Luis Response => ', luisresponse);
+        // let luisIntent = luisresponse.luisResult.prediction.topIntent;
+        // console.log(luisIntent);
         switch(stepContext.context.activity.text.toLowerCase()){
             case 'apply leave' : 
             return await stepContext.beginDialog(applyLeaveDialog);
@@ -48,7 +76,9 @@ class RootDialog extends ComponentDialog{
             return await stepContext.beginDialog(helpDialog);
 
             default : context.sendActivity('Sorry, I am still learning can you please refresh your query')
-        }
+        } 
+    }
+
 
         return await stepContext.endDialog();
 
